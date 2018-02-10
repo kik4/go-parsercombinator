7d5a82235d9e1212c704c25c413e15d515850bb8
kik4 <kikkikkikkik@users.noreply.github.com>
2018-02-11 01:14:51 +0900

    refactor: split file RuleFunc

--- parsercombinator.go
+++ parsercombinator.go
@@ -2,8 +2,6 @@ package parsercombinator
 
 import (
 	"errors"
-	"unicode"
-	"unicode/utf8"
 )
 
 type (
@@ -28,46 +26,6 @@ type (
 	// CountFunc is function counts RuleFunc.
 	CountFunc func(string) (string, int, error)
 )
-
-// AnyChar reads a charater.
-func AnyChar() RuleFunc {
-	return func(test string) (string, int, bool) {
-		if utf8.RuneCountInString(test) < 1 {
-			return "", 0, false
-		}
-
-		return test[:1], 1, true
-	}
-}
-
-// String validates equal test stiring.
-func String(s string) RuleFunc {
-	return func(test string) (string, int, bool) {
-		num := utf8.RuneCountInString(s)
-		if utf8.RuneCountInString(test) < num {
-			return "", 0, false
-		}
-		if test[0:num] == s {
-			return test[0:num], num, true
-		}
-		return "", 0, false
-	}
-}
-
-// Digit read a digit.
-func Digit() RuleFunc {
-	return func(test string) (string, int, bool) {
-		if utf8.RuneCountInString(test) < 1 {
-			return "", 0, false
-		}
-
-		c := test[:1][0]
-		if unicode.IsDigit(rune(c)) {
-			return test[:1], 1, true
-		}
-		return "", 0, false
-	}
-}
 
 // Once adapts parse rule once.
 func (rf RuleFunc) Once() Parser {
---
+++ rule.go
@@ -0,0 +1,46 @@
+package parsercombinator
+
+import (
+	"unicode"
+	"unicode/utf8"
+)
+
+// AnyChar reads a charater.
+func AnyChar() RuleFunc {
+	return func(test string) (string, int, bool) {
+		if utf8.RuneCountInString(test) < 1 {
+			return "", 0, false
+		}
+
+		return test[:1], 1, true
+	}
+}
+
+// String validates equal test stiring.
+func String(s string) RuleFunc {
+	return func(test string) (string, int, bool) {
+		num := utf8.RuneCountInString(s)
+		if utf8.RuneCountInString(test) < num {
+			return "", 0, false
+		}
+		if test[0:num] == s {
+			return test[0:num], num, true
+		}
+		return "", 0, false
+	}
+}
+
+// Digit read a digit.
+func Digit() RuleFunc {
+	return func(test string) (string, int, bool) {
+		if utf8.RuneCountInString(test) < 1 {
+			return "", 0, false
+		}
+
+		c := test[:1][0]
+		if unicode.IsDigit(rune(c)) {
+			return test[:1], 1, true
+		}
+		return "", 0, false
+	}
+}
