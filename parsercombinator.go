76cf35c7c228793e48ff2dacc3ce2e8bf8892cbf
kik4 <kikkikkikkik@users.noreply.github.com>
2018-02-11 01:38:48 +0900

    refactor: remove distinction of Teminal and NonTerminal

--- parsercombinator.go
+++ parsercombinator.go
@@ -6,33 +6,20 @@ import (
 
 type (
 	// Parser is parse executer.
-	Parser interface {
-		Parse(s string) (string, int, error)
-	}
-
-	// Terminal is terminal symbol parser.
-	Terminal struct {
-		f CountFunc
-	}
-
-	// NonTerminal is nonterminal symbol parser.
-	NonTerminal struct {
-		f CombineFunc
+	Parser struct {
+		f ParseFunc
 	}
 
 	// RuleFunc is function validates parse rule.
 	RuleFunc func(string) (string, int, bool)
 
-	// CountFunc is function counts RuleFunc.
-	CountFunc func(string) (string, int, error)
-
-	// CombineFunc is function conbines parser.
-	CombineFunc func(string) (string, int, error)
+	// ParseFunc is function parses.
+	ParseFunc func(string) (string, int, error)
 )
 
 // Once adapts parse rule once.
-func (rf RuleFunc) Once() Parser {
-	return &Terminal{f: func(test string) (string, int, error) {
+func (rf RuleFunc) Once() *Parser {
+	return &Parser{func(test string) (string, int, error) {
 		str, num, succeeded := rf(test)
 		if succeeded {
 			return str, num, nil
@@ -42,18 +29,13 @@ func (rf RuleFunc) Once() Parser {
 }
 
 // Parse executes parser
-func (t *Terminal) Parse(test string) (string, int, error) {
-	return t.f(test)
-}
-
-// Parse executes parser
-func (nt *NonTerminal) Parse(test string) (string, int, error) {
-	return nt.f(test)
+func (p *Parser) Parse(test string) (string, int, error) {
+	return p.f(test)
 }
 
 // Sequence combines Parsers
-func Sequence(parsers ...Parser) *NonTerminal {
-	return &NonTerminal{func(test string) (string, int, error) {
+func Sequence(parsers ...*Parser) *Parser {
+	return &Parser{func(test string) (string, int, error) {
 		content := make([]byte, 0)
 		read := 0
 
@@ -70,8 +52,8 @@ func Sequence(parsers ...Parser) *NonTerminal {
 }
 
 // Or selects matched parse result
-func Or(p1, p2 Parser) *NonTerminal {
-	return &NonTerminal{func(test string) (string, int, error) {
+func Or(p1, p2 *Parser) *Parser {
+	return &Parser{func(test string) (string, int, error) {
 		str, num, err := p1.Parse(test)
 		if err == nil {
 			return str, num, err
