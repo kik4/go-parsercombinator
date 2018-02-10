c12f45ae74af36780e72dab3387438020805337d
kik4 <kikkikkikkik@users.noreply.github.com>
2018-02-11 02:28:56 +0900

    feat: add test of Once

--- counter_test.go
+++ counter_test.go
@@ -2,6 +2,28 @@ package parsercombinator
 
 import "testing"
 
+func TestOnce(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		rule    RuleFunc
+		in      string
+		want1   string
+		want2   int
+		succeed bool
+	}{
+		{Letter(), "abc", "a", 1, true},
+		{Letter(), "3ff", "", 0, false},
+		{Letter(), "", "", 0, false},
+	}
+	for i, c := range cases {
+		got, num, err := c.rule.Once().Parse(c.in)
+		if !(got == c.want1 && num == c.want2) || !(c.succeed == (err == nil)) {
+			t.Error(i, got, num, err, c)
+		}
+	}
+}
+
 func TestAtLeastOnce(t *testing.T) {
 	t.Parallel()
 
@@ -14,6 +36,7 @@ func TestAtLeastOnce(t *testing.T) {
 	}{
 		{Letter(), "abc", "abc", 3, true},
 		{Letter(), "afa3ff", "afa", 3, true},
+		{Letter(), "3ff", "", 0, false},
 		{Letter(), "", "", 0, false},
 	}
 	for i, c := range cases {
