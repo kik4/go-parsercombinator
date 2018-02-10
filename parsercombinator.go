6111d3af241eab66bfd8e068666590693ad5d911
kik4 <kikkikkikkik@users.noreply.github.com>
2018-02-10 22:52:11 +0900

    feat: AnyChar and String

--- parsercombinator.go
+++ parsercombinator.go
@@ -1,6 +1,53 @@
 package parsercombinator
 
-// AnyChar get parse next char
-func AnyChar(s string) byte {
-	return s[0]
+import (
+	"errors"
+	"unicode/utf8"
+)
+
+type (
+	// Parser is parse executer.
+	Parser struct {
+		f CountFunc
+	}
+
+	// RuleFunc is function validates parse rule.
+	RuleFunc func(string) (string, int, bool)
+
+	// CountFunc is function counts RuleFunc.
+	CountFunc func(string) (string, error)
+)
+
+// AnyChar reads a charater.
+func AnyChar() RuleFunc {
+	return func(test string) (string, int, bool) {
+		return test[:1], 1, true
+	}
+}
+
+// String validates equal test stiring.
+func String(s string) RuleFunc {
+	return func(test string) (string, int, bool) {
+		num := utf8.RuneCountInString(s)
+		if test[0:num] == s {
+			return test[0:num], num, true
+		}
+		return "", 0, false
+	}
+}
+
+// Once adapts parse rule once.
+func (rf RuleFunc) Once() *Parser {
+	return &Parser{f: func(test string) (string, error) {
+		str, _, succeeded := rf(test)
+		if succeeded {
+			return str, nil
+		}
+		return "", errors.New("hoge")
+	}}
+}
+
+// Parse executes parser
+func (p *Parser) Parse(s string) (string, error) {
+	return p.f(s)
 }
--- parsercombinator_test.go
+++ parsercombinator_test.go
@@ -3,7 +3,15 @@ package parsercombinator
 import "testing"
 
 func TestAnyChar(t *testing.T) {
-	if AnyChar("test") != 't' {
-		t.Error()
+	s, error := AnyChar().Once().Parse("test")
+	if error != nil || s != "t" {
+		t.Error(error, s)
+	}
+}
+
+func TestString(t *testing.T) {
+	s, error := String("test").Once().Parse("test")
+	if error != nil || s != "test" {
+		t.Error(error, s)
 	}
 }
