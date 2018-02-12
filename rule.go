4b68fb55a3f11e182627b8c20e2fbb3c15c0062a
kik4 <kikkikkikkik@users.noreply.github.com>
2018-02-12 14:40:50 +0900

    refactor: common rule functions

--- rule.go
+++ rule.go
@@ -4,15 +4,14 @@ import (
 	"unicode"
 )
 
+type simpleIsFunc func(rune) bool
+
 // AnyChar reads a charater.
 func AnyChar() RuleFunc {
-	return func(test string) (string, int, bool) {
-		for _, r := range test {
-			str := string(r)
-			return str, len(str), true
-		}
-		return "", 0, false
+	fn := func(r rune) bool {
+		return true
 	}
+	return createCommonRuleFunc(fn)
 }
 
 // String validates equal test stiring.
@@ -31,37 +30,26 @@ func String(needle string) RuleFunc {
 
 // Digit read a digit.
 func Digit() RuleFunc {
-	return func(test string) (string, int, bool) {
-		for _, r := range test {
-			if !unicode.IsDigit(r) {
-				return "", 0, false
-			}
-			str := string(r)
-			return str, len(str), true
-		}
-		return "", 0, false
-	}
+	return createCommonRuleFunc(unicode.IsDigit)
 }
 
 // Letter read a Letter.
 func Letter() RuleFunc {
-	return func(test string) (string, int, bool) {
-		for _, r := range test {
-			if !unicode.IsLetter(r) {
-				return "", 0, false
-			}
-			str := string(r)
-			return str, len(str), true
-		}
-		return "", 0, false
-	}
+	return createCommonRuleFunc(unicode.IsLetter)
 }
 
 // Char read a rune assigned
 func Char(needle rune) RuleFunc {
+	fn := func(r rune) bool {
+		return r == needle
+	}
+	return createCommonRuleFunc(fn)
+}
+
+func createCommonRuleFunc(fn simpleIsFunc) RuleFunc {
 	return func(test string) (string, int, bool) {
 		for _, r := range test {
-			if r != needle {
+			if !fn(r) {
 				return "", 0, false
 			}
 			str := string(r)
